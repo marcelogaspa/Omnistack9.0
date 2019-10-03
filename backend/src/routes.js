34cb99fdb08f2d97eb74a5422c0e4cb82d8443ec
Marcelo Gasparetti <mgasparetti@daitan.com>
2019-10-03 14:44:38 -0300

    Finally I complete the backend!

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const SessionControler = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -14,5 +15,7 @@ routes.post('/sessions', SessionControler.store);
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
